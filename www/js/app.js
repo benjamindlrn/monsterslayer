6b52c476d63d385aab5e2989ef5709e6b3870148
root <benjamindelarosanajera@gmail.com>
2018-02-06 16:56:17 -0600

    Now the player can gain experience and Monster Selector UI created

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,66 @@
 var ModelView = function(){
-    this.isRunning = ko.observable(false);
+    this.isFighting = ko.observable(false);
     this.playerHealth = ko.observable(0);
     this.monsterHealth = ko.observable(0);
-    this.startGame = function() {
-        this.isRunning(true);
+    this.experience = ko.observable(0);
+    this.level = ko.observable(1);
+    this.nextLevel = ko.observable(30);
+    this.addExperience = function() {
+        this.experience(this.experience() + 10);
+        if (this.experience()>=this.nextLevel()) {
+            alert("Congratulations!! You leveled up!!");
+            this.level(this.level()+1);
+            this.nextLevel((this.nextLevel()*1.5).toFixed(0));
+        }
+    };
+
+    this.startFight = function() {
+        this.isFighting(true);
         this.playerHealth(100);
         this.monsterHealth(100);
     };
     this.giveUp = function(){
-        this.isRunning(false);
+        this.experience(0);
+        this.isFighting(false);
+        this.level(1);
+        this.nextLevel(30);
     };
     this.calculateDmg = function(max,min) {
         return Math.random() * (max - min) + min; 
     };
-
     this.monsterAttack = function() {
         this.playerHealth(this.playerHealth()-this.calculateDmg(14,10));        
     };
-
     this.attack = function(){
         this.monsterHealth(this.monsterHealth()-this.calculateDmg(12,6));
         if (this.monsterHealth()<=0) {
-            if (confirm("You won! New Game?")==true) {
-                this.startGame();
-                return;
-            }
-            this.isRunning(false);
+            alert("Victory! 10 Exp earned");
+            this.addExperience();
+            this.isFighting(false);
             return;
-        }        
+        }              
         this.monsterAttack(); 
+        if (this.playerHealth()<=0) {
+            alert("You lose... Try again!!");
+            this.experience(0);
+            this.isFighting(false);
+        }  
     };
 
     this.specialAttack = function(){
         this.monsterHealth(this.monsterHealth()-this.calculateDmg(18,10));
         if (this.monsterHealth()<=0) {
-            if (confirm("You won! New Game?")==true) {
-                this.startGame();
-                return;
-            }
-            this.isRunning(false);
+            alert("Victory! 10 Exp earned");
+            this.addExperience();
+            this.isFighting(false);
             return;
-        }        
-        this.monsterAttack();
+        }              
+        this.monsterAttack(); 
+        if (this.playerHealth()<=0) {
+            alert("You lose... Try again!!");
+            this.experience(0);
+            this.isFighting(false);
+        }
     };
 
     this.heal = function() {
